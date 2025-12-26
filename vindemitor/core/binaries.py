711149846169474fadd623d3deaa54757b320254
Shivelight <shivelight@tuta.io>
2025-12-26 21:43:56 +0700

    feat: add linux to possible shaka-packager platform, export MkvMerge and MkvPropEdit

--- vindemitor/core/binaries.py
+++ vindemitor/core/binaries.py
@@ -3,11 +3,6 @@ import sys
 from pathlib import Path
 from typing import Optional
 
-__shaka_platform = {
-    "win32": "win",
-    "darwin": "osx"
-}.get(sys.platform, sys.platform)
-
 
 def find(*names: str) -> Optional[Path]:
     """Find the path of the first found binary name."""
@@ -24,28 +19,26 @@ FFPlay = find("ffplay")
 MkvMerge = find("mkvmerge")
 MkvPropEdit = find("mkvpropedit")
 SubtitleEdit = find("SubtitleEdit")
+__shaka_platform = {"win32": "win", "darwin": "osx", "linux": "linux"}.get(sys.platform, sys.platform)
 ShakaPackager = find(
     "shaka-packager",
     "packager",
     f"packager-{__shaka_platform}",
     f"packager-{__shaka_platform}-arm64",
-    f"packager-{__shaka_platform}-x64"
+    f"packager-{__shaka_platform}-x64",
 )
 Mp4Decrypt = find("mp4decrypt")
 Aria2 = find("aria2c", "aria2")
-CCExtractor = find(
-    "ccextractor",
-    "ccextractorwin",
-    "ccextractorwinfull"
-)
+CCExtractor = find("ccextractor", "ccextractorwin", "ccextractorwinfull")
 HolaProxy = find("hola-proxy")
 MPV = find("mpv")
-
 
 __all__ = (
     "FFMPEG",
     "FFProbe",
     "FFPlay",
+    "MkvMerge",
+    "MkvPropEdit",
     "SubtitleEdit",
     "ShakaPackager",
     "Mp4Decrypt",
